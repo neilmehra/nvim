5ad35b592980049437535fb11f47c6c28007876c
neil mehra <neilmehra@outlook.com>
2025-04-22 17:23:54 -0400

    image nvim update

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -11,8 +11,8 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "gitcommit", "markdown" },
   callback = function()
-    -- vim.opt_local.wrap = false
-    -- need to turn this off for image.nvim to work properly
+    -- image.nvim should be fixed now
+    vim.opt_local.wrap = true
     vim.opt_local.spell = true
   end,
 })
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,7 +2,7 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     event = "VeryLazy",
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {
@@ -20,16 +20,17 @@ return {
         markdown = {
           enabled = true,
           clear_in_insert_mode = false,
-          download_remote_images = true,
+          download_remote_images = false,
           only_render_image_at_cursor = true,
-          filetypes = { "markdown", }, -- markdown extensions (ie. quarto) can go here
+          only_render_image_at_cursor_mode = "inline",
+          floating_windows = false, -- if true, images will be rendered in floating markdown windows
+          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
         },
       },
       max_width = nil,
       max_height = nil,
       max_width_window_percentage = nil,
       max_height_window_percentage = 50,
-      kitty_method = "normal",
     },
   },
 }
