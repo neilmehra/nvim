f618e4dba776b3bceb66d77428db2592f95b96ae
neil mehra <neilmehra@outlook.com>
2026-05-21 18:16:37 -0700

    treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,42 +1,37 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdateSync" },
-    dependencies = {
-      { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
-      "windwp/nvim-ts-autotag",
-    },
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash",
-          "cmake",
-          "c",
-          "cpp",
-          "diff",
-          "gitignore",
-          "git_rebase",
-          "gitcommit",
-          "lua",
-          "luadoc",
-          "make",
-          "html",
-          "json",
-          "python",
-          "rust",
-          "markdown",
-          "markdown_inline",
-          "latex",
-          "vim",
-          "vimdoc",
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-        autotag = { enable = true },
+      local parsers = {
+        "bash", "cmake", "c", "cpp", "diff",
+        "gitignore", "git_rebase", "gitcommit",
+        "lua", "luadoc", "make", "html", "json",
+        "python", "rust", "markdown", "markdown_inline",
+        "latex", "vim", "vimdoc",
+      }
+
+      require("nvim-treesitter").install(parsers)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(ev)
+          local ft = vim.bo[ev.buf].filetype
+          local lang = vim.treesitter.language.get_lang(ft)
+          if not lang then return end
+          local ok, added = pcall(vim.treesitter.language.add, lang)
+          if not (ok and added) then return end
+          pcall(vim.treesitter.start, ev.buf, lang)
+          vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
     end,
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {},
   },
 }
